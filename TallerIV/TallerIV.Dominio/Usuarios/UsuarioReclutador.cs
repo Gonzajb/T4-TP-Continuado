a345de5eeac40a58ab5cbc58d197e7d54fc0b44a
Gonzalo <gonzajb@gmail.com>
2017-09-11 22:21:16 -0300

    registro usuario reclutador

--- TallerIV/TallerIV.Dominio/Usuarios/UsuarioReclutador.cs
+++ TallerIV/TallerIV.Dominio/Usuarios/UsuarioReclutador.cs
@@ -8,6 +8,14 @@ namespace TallerIV.Dominio
 {
     public class UsuarioReclutador: UsuarioPersona
     {
+        public UsuarioReclutador() { }
+
+        public UsuarioReclutador (DateTime fechaDeResgistro, string email, string userName, string nombre, string apellido, DateTime? fechaDeNacimiento) : base(fechaDeResgistro, email, userName,nombre,apellido,fechaDeNacimiento)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            FechaDeNacimiento = fechaDeNacimiento;
+        }
         //public List<Aviso>Avisos { get; set; }
         public string UsuarioEmpresa_Id { get; set; }
     }
