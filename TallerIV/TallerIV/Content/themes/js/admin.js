2369b0162581a286ce0f01d03508eddc013f043b
Brian Capo <39756169@A-CVI-05>
2017-09-27 22:19:52 -0300

    Niño Template

---
+++ TallerIV/TallerIV/Content/themes/js/admin.js
@@ -0,0 +1,7 @@
+﻿$(function () {
+    $(".numeric").numeric();
+    $(".integer").numeric(false, function () { this.value = ""; this.focus(); });
+    $(".positive").numeric({ negative: false }, function () { this.value = ""; this.focus(); });
+    $(".positive-integer").numeric({ decimal: false, negative: false }, function () { this.value = ""; this.focus(); });
+    $(".decimal-2-places").numeric({ decimalPlaces: 2 });
+});
---
+++ TallerIV/TallerIV/Content/themes/js/functions.js
@@ -0,0 +1,41 @@
+﻿function showSuccess(message) {
+    $('div#modal-success div.modal-body').html(message);
+    $('div#modal-success').modal('show');
+}
+
+function hideSuccess() {
+    $('div#modal-success div.modal-body').html("");
+    $('div#modal-success').modal('hide');
+}
+
+function showError(message)
+{
+    $('div#modal-error div.modal-body').html(message);
+    $('div#modal-error').modal('show');
+}
+
+function hideError() {
+    $('div#modal-error div.modal-body').html("");
+    $('div#modal-error').modal('hide');
+}
+
+function showLoading()
+{
+    $('div#modal-loading').modal('show');
+}
+
+function hideLoading() {
+    $('div#modal-loading').modal('hide');
+}
+
+function showConfirm(message, callback) {
+    $('div#modal-confirm div.modal-body').html(message);
+    $('div#modal-confirm').modal('show');
+
+    $('div#modal-confirm div.modal-footer a.btn-confirm').click(callback);
+}
+
+function hideConfirm() {
+    $('div#modal-confirm div.modal-body').html("");
+    $('div#modal-confirm').modal('hide');
+}
