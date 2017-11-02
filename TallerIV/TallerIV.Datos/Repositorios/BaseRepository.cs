e248b1eea889a7fc16154d12737542e6ba952664
Nicolas Sabaj <nsabaj@hotmail.com>
2017-11-02 13:51:09 -0300

    agregado bootbox

--- TallerIV/TallerIV.Datos/Repositorios/BaseRepository.cs
+++ TallerIV/TallerIV.Datos/Repositorios/BaseRepository.cs
@@ -26,23 +26,38 @@ namespace TallerIV.Datos.Repositorios
             dbSet.Add(entity);
             db.SaveChanges();
         }
+        /// <summary>  
+        ///  Esta función agrega a la base de datos el listado de entidades de tipo genérico recibido por parámetro.
+        /// </summary>  
         public void AddEntityRange(List<T> entities)
         {
             dbSet.AddRange(entities);
             db.SaveChanges();
         }
+        /// <summary>  
+        ///  Esta función elimina de la base de datos la entidad de tipo genérico recibida por parámetro.
+        /// </summary>  
         public void RemoveEntity(T entity)
         {
             dbSet.Remove(entity);
             db.SaveChanges();
         }
+        /// <summary>  
+        ///  Esta función retorna una query de base de datos que selecciona todas las entidades del tipo genérico.
+        /// </summary>  
         public IQueryable<T> GetAll()
         {
             return dbSet;
         }
+        /// <summary>  
+        ///  Esta función retorna la entidad genérica obtenida de la base de datos relacionada al id que recibe por parámetro.
+        /// </summary>  
         public T GetById(long id) {
             return dbSet.Find(id);
         }
+        /// <summary>  
+        ///  Esta función actualiza en la base de datos la entidad que recibe por parámetro del tipo genérico.
+        /// </summary>  
         public void UpdateEntity(T entity) {
             dbSet.AddOrUpdate(entity);
             db.SaveChanges();
