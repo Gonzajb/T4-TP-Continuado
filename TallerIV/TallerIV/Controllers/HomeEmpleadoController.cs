1852115fd6b6fe9232391b44f6592f67ccdb53c4
39756169 <39756169@A-CFU-02>
2017-11-08 22:58:46 -0300

    avisos

--- TallerIV/TallerIV/Controllers/HomeEmpleadoController.cs
+++ TallerIV/TallerIV/Controllers/HomeEmpleadoController.cs
@@ -9,15 +9,20 @@ namespace TallerIV.Controllers
 {
     public class HomeEmpleadoController : Controller
     {
-        
+
         // GET: HomeEmpleado
         private AvisosService avisoService;
         public ActionResult Index()
         {
             avisoService = new AvisosService();
-            
+
             return View(avisoService.GetAll(false).ToList());
         }
+        
+        public string Like(int id)
+        {
+            return "hola";
+        }    
 
     }
 }
