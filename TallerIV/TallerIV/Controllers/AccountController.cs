62363de3e8b95f8af460223e9c85ad289ab51d06
Gonzalo <gonzajb@gmail.com>
2017-11-12 13:13:26 -0300

    d

--- TallerIV/TallerIV/Controllers/AccountController.cs
+++ TallerIV/TallerIV/Controllers/AccountController.cs
@@ -84,7 +84,8 @@ namespace TallerIV.Controllers
             switch (result)
             {
                 case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
+                    return RedirectToAction("Index", "Home");
+                    //return RedirectToLocal(returnUrl);
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
